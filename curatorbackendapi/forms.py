debde5dee3ab76915f71bd4570c5ba641762054d
fazer1929 <fazer1929@users.noreply.github.com>
2022-02-20 15:02:33 +0000

    :art: Format Python code with psf/black

--- curatorbackendapi/forms.py
+++ curatorbackendapi/forms.py
@@ -1,21 +1,23 @@
 from django import forms
+
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 
-from django.contrib.auth.forms import UserCreationForm  
-from django.core.exceptions import ValidationError  
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
-        model=get_user_model()
+        model = get_user_model()
         fields = ("email", "first_name", "last_name", "password1", "password2")
 
     def clean_email(self):
-        User=get_user_model()
-        email = self.cleaned_data['email'].lower()
-        new = User.objects.filter(email=email)  
-        if new.count():  
+        User = get_user_model()
+        email = self.cleaned_data["email"].lower()
+        new = User.objects.filter(email=email)
+        if new.count():
             raise ValidationError("Email Already Exist")
         return email
