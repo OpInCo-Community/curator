c26f0d3f1ab066e3c81398d73aca2a352a142679
rohitgeddam <rohitgeddam@users.noreply.github.com>
2022-02-20 03:39:14 +0000

    :art: Format Python code with psf/black

--- curations/migrations/0002_alter_curation_owner.py
+++ curations/migrations/0002_alter_curation_owner.py
@@ -9,13 +9,17 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('curations', '0001_initial'),
+        ("curations", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='curation',
-            name='owner',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='curations', to=settings.AUTH_USER_MODEL),
+            model_name="curation",
+            name="owner",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="curations",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
